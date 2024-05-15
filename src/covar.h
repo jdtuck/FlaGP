41b00404fcbdbdfadd9408feb55cf2c452fb3ee4
granthutchings <43142979+granthutchings@users.noreply.github.com>
2024-05-15 13:30:22 -0600

    remove additional unused C code
    removing C files and functions that are not called

--- src/covar.h
+++ src/covar.h
@@ -11,22 +11,5 @@ void covar(const int col, double **X1, const int n1, double **X2,
 void covar_R(int *col_in, double *X1_in, int *n1_in, double *X2_in,
              int *n2_in, double *d_in, double *K_in);
 //
-void diff_covar_symm(const int col, double **X, const int n,
-                     double d, double **dK, double **d2K);
-void diff_covar(const int col, double **X1, const int n1, double **X2,
-		const int n2, double d, double **dK, double **d2K);
-void distance(double **X1, const unsigned int n1, double **X2,
-	      const unsigned int n2, const unsigned int m, double **D);
-void distance_R(double *X1_in, int *n1_in, double *X2_in,
-		int *n2_in, int *m_in, double *D_out);
-void distance_symm_R(double *X_in, int *n_in, int *m_in, double *D_out);
-void dist2covar_R(double *D_in, int *n1_in, int *n2_in, double *d_in,
-		  double *g_in, double *K_out);
-void dist2covar_symm_R(double *D_in, int *n_in, double *d_in,
-		       double *g_in, double *K_out);
-void calc_g_mui_kxy(const int col, double *x, double **X,
-		    const int n, double **Ki, double **Xref,
-		    const int m, double d, double g, double *gvec,
-        	    double *mui, double *kx, double *kxy);
 #endif
 
